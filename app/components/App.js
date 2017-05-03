f4b6493219bec58a5259c037692bd440d07c4b73
Rafalcho <rafalcho@github.com>
2017-05-03 11:16:27 +0200

    home component

--- app/components/App.js
+++ app/components/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Popular from './Popular';
-import ReactRouter, {Router, Route, Switch} from 'react-router-dom';
+import ReactRouter, {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import {Nav} from './Nav';
+import {Home} from './Home';
 
 class App extends React.Component {
   render() {
     return (
       <Router>
         <div className='container'>
+          <Nav />
+          <Route path='/' component={Home} />
           <Route path='/popular' component={Popular} />
         </div>
       </Router>
